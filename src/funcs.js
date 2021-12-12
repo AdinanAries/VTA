43ffd64b9ac460e8e7a6069a8cdbc18a26845c66
Mohammed Adinan <adinanaries@outlook.com>
2021-12-12 09:33:36 -0500

    now cleaning up queries

--- src/funcs.js
+++ src/funcs.js
@@ -1,12 +1,27 @@
 const dialogs = require("../data/dialogs.json");
 
+//function for replying to query
+//may contain sub-functions
+/*---Interface---*/
 const talk = () => {
-    console.log(dialogs);
+    //console.log(dialogs);
+    console.log(evaluate("What are you doing"))
 }
 
 //function to evaluate request from user
 //may contain sub-functions
-const evaluate = () => {
+/*---Interface---*/
+const evaluate = (submited_query) => {
+
+    //1. turn query to array
+    const words = submited_query.split(" ");
+    //2. removing noise words
+    const key_words = words.filter(each=>{
+        return !dialogs.Noises.includes(each);
+    });
+
+
+    console.log(key_words);
     console.log("function to evaluate request from user")
 }
 
