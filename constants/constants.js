bfc669bb7a990e34c4f6049b16fa9c00db949d38
Mohammed Adinan <adinanaries@outlook.com>
2024-07-16 21:39:19 -0400

    adding a third party welldugo server url

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,5 @@
 let constants = {
+    THIRD_PARTY_WELLDUGO_SERVER_URL: "https://ai-welldugo-120e1d6a8826.herokuapp.com/",
     AGENT_STATE_NAMES: {
         begin_air_booking: "begin_air_booking",
         help_options: "help_options",
